585e307c185910ac1a0013ded3ad5db35b1085c6
Ian Johnson <ijohnson@wolfram.com>
2017-06-26 15:52:55 -0400

    Adding initial commit of the link source file

---
+++ project/CSource/daqlink.c
@@ -0,0 +1,44 @@
+
+#include "WolframLibrary.h"
+
+
+EXTERN_C DLLEXPORT int CreateTask(WolframLibraryData libData, mint argc, MArgument* args, MArgument res)
+{
+	//get the name string as the name to use from the first argument
+	char * nameString = MArgument_getUTF8String(args[0]);
+
+	//allocate a task here
+	TaskHandle * task = (TaskHandle *)calloc(1,sizeof(TaskHandle));
+
+	//create the task with the api function from daq
+	int error = DAQmxCreateTask(nameString,task);
+	if(error != 0)
+	{
+		//error - failed
+		return LIBRARY_FUNCTION_ERROR;
+	}
+
+	//success
+	return LIBRARY_NO_ERROR;
+}
+
+
+EXTERN_C DLLEXPORT int LoadTask(WolframLibraryData libData, mint argc, MArgument* args, MArgument res)
+{
+	//get the name string as the name to use from the first argument
+	char * nameString = MArgument_getUTF8String(args[0]);
+
+	//allocate a task here
+	TaskHandle * task = (TaskHandle *)calloc(1,sizeof(TaskHandle));
+
+	//create the task with the api function from daq
+	int error = DAQmxLoadTask(nameString,task);
+	if(error != 0)
+	{
+		//error - failed
+		return LIBRARY_FUNCTION_ERROR;
+	}
+
+	//success
+	return LIBRARY_NO_ERROR;
+}
